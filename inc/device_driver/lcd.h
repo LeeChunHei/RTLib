bf4f852db3fffc6ec25a40711937e4741296228a
LeeChunHei <lch19980829@gmail.com>
2018-09-25 23:40:58 +0800

    lcd class updated

--- inc/device_driver/lcd.h
+++ inc/device_driver/lcd.h
@@ -40,12 +40,8 @@ public:
 	virtual void FillColorPixel(const uint16_t* pixel, const uint32_t length) = 0;
 	virtual void FillColorPixel(const uint32_t* pixel, const uint32_t length) = 0;
 	virtual void FillBits(const uint32_t color_t, const uint32_t color_f, const uint8_t *data, const uint32_t bit_length) = 0;
-	void DrawLine(const uint32_t start_x, const uint32_t start_y, const uint32_t end_x, const uint32_t end_y, const uint32_t width = 1) {
-
-	}
-	void DrawCircle(const uint32_t x, const uint32_t y, const uint32_t radius, const uint32_t width = 1, const bool color_filled = false, const uint32_t fill_color = 0) {
-
-	}
+	void DrawLine(const uint32_t start_x, const uint32_t start_y, const uint32_t end_x, const uint32_t end_y, const uint32_t color, const uint32_t width = 1);
+	void DrawCircle(const uint32_t x, const uint32_t y, const uint32_t radius, const uint32_t boundary_color, const uint32_t width = 1, const bool color_filled = false, const uint32_t fill_color = 0);
 	virtual void Clear() = 0;
 	virtual void Clear(const uint32_t color) = 0;
 };
